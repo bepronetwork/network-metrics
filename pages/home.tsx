6b539b78c6f2bcefbe2b3b517c5f1e50f58efeb4
moshmage <moshmage@gmail.com>
2021-10-28 17:35:36 +0100

    make transactional-data.tsx

--- pages/home.tsx
+++ pages/home.tsx
@@ -5,8 +5,7 @@ import NavBar from '@components/nav-bar';
 import PageHeader from '@components/page-header';
 import CirculatingSupply from '@components/circulating-supply';
 import DistributedEpoch from '@components/distributed-epoch';
-import IconUSDC from '@icons/IconUSDC';
-import Circle from '@components/Circle';
+import TransactionalData from '@components/transactional-data';
 
 export default function HomePage() {
   return <>
@@ -21,18 +20,7 @@ export default function HomePage() {
         <Column className="bg-shadow">
           <h4 className="mb-3">Protocol metrics</h4>
           <Row>
-            <Column className="bg-dark-gray">
-              <h4 className="d-flex justify-content-between color-white mb-3">
-                <span>Transactional</span>
-                <Circle className="color-white bg-primary" width={24} height={24}><IconUSDC /></Circle>
-              </h4>
-              <Row>
-                <Column size={3} className="text-center border-right border-light-dark-gray py-2 px-0">4.2M</Column>
-                <Column size={3} className="text-center border-right border-light-dark-gray py-2 px-0">42K</Column>
-                <Column size={3} className="text-center border-right border-light-dark-gray py-2 px-0">$4.2M</Column>
-                <Column size={3} className="text-center py-2 px-0">4.2%</Column>
-              </Row>
-            </Column>
+            <TransactionalData />
             <Column>hey</Column>
           </Row>
         </Column>
